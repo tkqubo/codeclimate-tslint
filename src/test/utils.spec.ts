549ba761948d836f4558d3d151c93369c6918c9c
tkqubo <tk.qubo@gmail.com>
2018-02-02 20:47:53 +0900

    fix: test

--- src/test/utils.spec.ts
+++ src/test/utils.spec.ts
@@ -1,9 +1,11 @@
 'use strict';
 
-const mock = require('mock-fs');
-const assert = require('power-assert');
+import {RuleNameNotFoundError} from '../issueConverter';
 import * as CodeClimate from '../codeclimateDefinitions';
 import Utils from '../utils';
+
+const mock = require('mock-fs');
+const assert = require('power-assert');
 
 describe('Utils', () => {
   describe('createEmptyRuleMetadata(ruleName: string)', () => {
@@ -17,9 +19,9 @@ describe('Utils', () => {
   describe('createIssueFromError(e: Error)', () => {
     it('creates correct instance', done => {
       const path = 'some path';
-      const e = new Error('error message');
+      const e = new RuleNameNotFoundError('some-rule');
       const actual = Utils.createIssueFromError(e, path);
-      assert.equal(actual.description, `${e.name}: ${e.message}\n${e.stack}`);
+      assert.equal(actual.description, `Sorry, description could not be provided due to the internal error:\n${e.stack}`);
       assert.equal(actual.type, CodeClimate.issueTypes.Issue);
       done();
     });
